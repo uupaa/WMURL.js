b80756192f9808d6130e8662c7cb45d23b2e88c0
obara.takao <obara.takao@dena.jp>
2014-09-06 03:49:18 +0900

    remove WMURL.encode.js, WMURL.decode.js to build settings

--- test/index.node.js
+++ test/index.node.js
@@ -6,7 +6,5 @@ require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
 require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../lib/WMURL.js");
-require("../lib/WMURL.encode.js");
-require("../lib/WMURL.decode.js");
 require("../release/WMURL.min.js");
 require("./test.js");
